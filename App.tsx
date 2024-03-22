aadf10d0fcf2a406074061942ae6d19b4c85d373
{ Walyson } <walysonmoura222@gmail.com>
2024-03-22 22:48:27 +0000

    new

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import 'react-native-gesture-handler';
-import { config } from '@gluestack-ui/config';
-import { GluestackUIProvider } from '@gluestack-ui/themed';
-import RootStack from './src/navigation';
-import { StatusBar, View } from 'react-native';
+import { GluestackUIProvider, Text, View } from "@gluestack-ui/themed";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { Loading } from '~/components/Loading';
-import { THEME } from '~/theme';
+import React from "react";
+import { config, gluestackUIConfig } from "@/components/gluestack-ui.config";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <GluestackUIProvider config={THEME}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <RootStack /> : <Loading />}
+    <GluestackUIProvider config={config}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <View>
+        <Text color="$green700">gg</Text>
+      </View>
     </GluestackUIProvider>
   );
 }
