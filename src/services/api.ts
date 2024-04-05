b72a4cff22c30da10402bc5769904f72941cc9e5
{ Walyson } <walysonmoura222@gmail.com>
2024-04-05 18:55:28 +0000

    new

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,34 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
+
+type SignOut = () => void;
+
+type APIInstaceProps = AxiosInstance & {
+  registerInterceptTokenManeger: (signOut: SignOut) => () => void;
+};
 
 const api = axios.create({
   baseURL: 'https://api-ignite-gym.vercel.app/',
-});
+}) as APIInstaceProps;
+
+let isRefreshing = false;
+
+api.registerInterceptTokenManeger = (signOut) => {
+  const interceptTokenManeger = api.interceptors.response.use(
+    (response) => response,
+    async (requestError) => {
+      if (requestError.response?.status === 401) {
+        if (
+          requestError.response.data?.message === 'token.expired' ||
+          requestError.response.data?.message === 'token.invalid'
+        ) {
+        }
+      }
+    }
+  );
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManeger);
+  };
+};
 
 export { api };
-
-
